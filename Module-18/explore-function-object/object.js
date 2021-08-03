3536243bc31219decb693491c6931129fe7c3ba1
habibshrabon <snhabib777@gmail.com>
2021-08-03 21:20:04 +0600

    just object starting study

---
+++ Module-18/explore-function-object/object.js
@@ -0,0 +1,16 @@
+var student1 = { id: 115, name: "Ahsan", marks: 81, phone: 369258 };
+
+var mobile = {
+  colors: "black",
+  price: 19000,
+  screenSize: 5,
+  storages: "16gb",
+};
+
+var book = {
+  title: "Start with why",
+  author: "Simon sinek",
+  price: 200,
+  pages: 246,
+  publisher: "penguin",
+};
