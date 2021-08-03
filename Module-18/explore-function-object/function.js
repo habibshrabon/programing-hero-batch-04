aefbb4fa5ea59b585ca76d6cf727ef141c7b8965
habibshrabon <snhabib777@gmail.com>
2021-08-03 20:54:41 +0600

    Function er parameter study

---
+++ Module-18/explore-function-object/function.js
@@ -0,0 +1,17 @@
+// function declation
+function startFan() {
+  console.log("walk towards the switch");
+  console.log("press the switch");
+}
+
+//call the function
+startFan();
+
+console.log("it breakfast");
+console.log("Drink tear down");
+console.log("take a shower");
+startFan();
+
+var country = "bd";
+var city = "Dhaka";
+startFan();
