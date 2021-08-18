74352a587b9faaf40a5e50efc2c86d23f4e0ee30
habibshrabon <snhabib777@gmail.com>
2021-08-18 23:53:40 +0600

    case number plus and minus add event handeler complete

---
+++ Module-27/shoppeng-cart-solution/app.js
@@ -0,0 +1,17 @@
+function updateCaseNumber(isIncreasing) {
+  const caseInput = document.getElementById("case-number");
+  const caseNumber = caseInput.value;
+  if (isIncreasing) {
+    caseInput.value = parseInt(caseNumber) + 1;
+  } else if (caseNumber > 0) {
+    caseInput.value = parseInt(caseNumber) - 1;
+  }
+}
+
+document.getElementById("case-plus").addEventListener("click", function () {
+  updateCaseNumber(true);
+});
+
+document.getElementById("case-minus").addEventListener("click", function () {
+  updateCaseNumber(false);
+});
