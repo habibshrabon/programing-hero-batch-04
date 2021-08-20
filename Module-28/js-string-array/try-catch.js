6525efef48b2c60db9735123e678000a05dd66d3
habibshrabon <snhabib777@gmail.com>
2021-08-20 16:12:24 +0600

    Try-catch

---
+++ Module-28/js-string-array/try-catch.js
@@ -0,0 +1,18 @@
+let myName = "ami";
+const students = undefined;
+// console.log(students.name);
+
+try {
+  myName = "tumi";
+} catch (error) {
+  console.log("getting error", error);
+} finally {
+}
+console.log(100);
+console.log(myName);
+
+// for(let i = 0; i < numbers.length i++){
+
+// }
+// name = "tumi";
+// console.log(name);
