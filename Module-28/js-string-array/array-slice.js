6d34eb7e910d41332a703231fa3ac033afffcd0b
habibshrabon <snhabib777@gmail.com>
2021-08-20 15:22:02 +0600

    get part of an array and insert elements using slice and splice

---
+++ Module-28/js-string-array/array-slice.js
@@ -0,0 +1,15 @@
+const numbers = [3, 6, 4, 8, 9, 19, 15, 21, 45, 87];
+//slice
+const numberSliced = numbers.slice(4, 8);
+// console.log(numberSliced);
+// console.log(numbers);
+
+//splice
+// const numberSpliced = numbers.splice(4, 2);
+// console.log(numberSpliced);
+// console.log(numbers);
+
+//
+const numberSpliced2 = numbers.splice(4, 3, 777);
+console.log(numberSpliced2);
+console.log(numbers);
