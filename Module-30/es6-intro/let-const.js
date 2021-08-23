3c907292e967d45936f79880025705045aeb81cc
habibshrabon <snhabib777@gmail.com>
2021-08-23 12:31:48 +0600

    let and const

---
+++ Module-30/es6-intro/let-const.js
@@ -0,0 +1,20 @@
+let balance = 1240;
+balance = 1340;
+
+const userName = "janpahki ptas ptaoa";
+const weTogether = "ami" + userName;
+console.log(weTogether);
+// userName = "moyna";
+
+const numbers = [45, 23, 89, 60];
+// numbers = [99, 145, 3];//not allowed
+numbers.push(55);
+
+const students = { roll: 101, name: "John" };
+students.name = "mofiz";
+// students = { name: "Joni", roll: 555 };
+
+for (let i = 0; i < numbers.length; i++) {
+  const number = numbers[i];
+  console.log(number);
+}
