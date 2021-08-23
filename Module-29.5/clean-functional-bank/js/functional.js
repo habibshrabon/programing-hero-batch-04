740ea21a062d59e7035ed77829d972e4284cf2f4
habibshrabon <snhabib777@gmail.com>
2021-08-23 10:12:21 +0600

    Error handling more functions different balance

--- Module-29.5/clean-functional-bank/js/functional.js
+++ Module-29.5/clean-functional-bank/js/functional.js
@@ -6,32 +6,43 @@ function getInputValue(fieldId) {
   return value;
 }
 
+function getInnerTextValue(fieldId) {
+  const fieldTag = document.getElementById(fieldId);
+  const fieldValueText = fieldTag.innerText;
+  const value = parseFloat(fieldValueText);
+  return value;
+}
+
 function updateTotal(fieldId, amount) {
-  const totalTag = document.getElementById(fieldId);
-  const previousTotalInText = totalTag.innerText;
-  const previousTotal = parseFloat(previousTotalInText);
+  //   const totalTag = document.getElementById(fieldId);
+  //   const previousTotalInText = totalTag.innerText;
+  //   const previousTotal = parseFloat(previousTotalInText);
+  const previousTotal = getInnerTextValue(fieldId);
   const newTotal = previousTotal + amount;
-  totalTag.innerText = newTotal;
+  document.getElementById(fieldId).innerText = newTotal;
 }
 
 function updateBalance(amount, isAdding) {
-  const balanceTag = document.getElementById("balance-total");
-  const balanceInText = balanceTag.innerText;
-  const previousBalance = parseFloat(balanceInText);
+  //   const balanceTag = document.getElementById("balance-total");
+  //   const balanceInText = balanceTag.innerText;
+  //   const previousBalance = parseFloat(balanceInText);
+  const previousBalance = getInnerTextValue("balance-total");
   let newBalance;
   if (isAdding == true) {
     newBalance = previousBalance + amount;
   } else {
     newBalance = previousBalance - amount;
   }
-  balanceTag.innerText = newBalance;
+  document.getElementById("balance-total").innerText = newBalance;
 }
 document
   .getElementById("deposit-button")
   .addEventListener("click", function () {
     const amount = getInputValue("deposit-input");
-    updateTotal("deposit-total", amount);
-    updateBalance(amount, true);
+    if (amount > 0) {
+      updateTotal("deposit-total", amount);
+      updateBalance(amount, true);
+    }
   });
 
 //handel withdraw
@@ -39,6 +50,9 @@ document
   .getElementById("withdraw-button")
   .addEventListener("click", function () {
     const amount = getInputValue("withdraw-input");
-    updateTotal("withdraw-total", amount);
-    updateBalance(amount, false);
+    const balance = getInnerTextValue("balance-total");
+    if (amount > 0 && amount <= balance) {
+      updateTotal("withdraw-total", amount);
+      updateBalance(amount, false);
+    }
   });
