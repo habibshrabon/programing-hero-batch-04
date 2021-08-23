b1a74a59cfbddba201b593738951cfe7fb9e37c4
habibshrabon <snhabib777@gmail.com>
2021-08-23 23:44:26 +0600

    just commit

---
+++ Module-30/es6-intro/template-string.js
@@ -0,0 +1,25 @@
+const priya = "Asif akbar";
+const meye = "Meye tumi ki dukko chino";
+const kobita = `kobita tumi sopno charini`;
+const multiline =
+  "This is my first line \n" +
+  "this is my second line \n" +
+  "this is my third line \n" +
+  "this is my fourth line \n" +
+  "this is my fifth line";
+
+const multilineNew = `this is multi line 
+  this is special line
+  this is a line`;
+
+const friends = ["abul", "babul", "kabul"];
+const count = 5;
+const old = '<h3 class="friend-name">Friend -4</h3>';
+const old2 = '<h3 class="friend-name">Friend -' + count + "</h3>";
+const new1 = `<h3 class="friend-name">Friend -${friends.length}</h3>`;
+
+const first = "Mamun";
+const last = "Chowdhury";
+const fullOld = first + " " + last;
+const fullNew = `${first} ${last}`;
+console.log(fullNew);
