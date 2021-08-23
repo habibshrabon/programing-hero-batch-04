63141f1a5f4f696bc7fefa0315b2841da7e15b80
habibshrabon <snhabib777@gmail.com>
2021-08-24 00:11:11 +0600

    More arrow function

---
+++ Module-30/es6-intro/big-arrow.js
@@ -0,0 +1,28 @@
+const add = (num1, num2) => num1 + num2;
+const sum = add(22, 90);
+
+const multiply = (num1, num2, num3) => num1 * num2 * num3;
+const result = multiply(4, 12, 3);
+
+const fiveTimes = (num) => num * 5;
+const outPut = fiveTimes(17);
+
+const tenTimes = (num) => num * 10;
+const value = tenTimes(17);
+
+const getName = () => "Brandon sam";
+const name = getName();
+console.log(name);
+
+const doMath = (x, y) => {
+  const sum = x + y;
+  const diff = x - y;
+  const result = sum + diff;
+  const outPut = result * 5;
+  return outPut;
+};
+const total = doMath(12, 5);
+console.log(total);
+
+
+document.getElementById("my-btn").addEventListener(event=>)
