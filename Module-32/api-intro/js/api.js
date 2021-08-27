c8147803c5e34f2abdf3bb8d64f1394b37c29292
habibshrabon <snhabib777@gmail.com>
2021-08-28 01:04:17 +0600

    Load more data,More api

--- Module-32/api-intro/js/api.js
+++ Module-32/api-intro/js/api.js
@@ -1,5 +1,21 @@
 function loadData() {
   fetch("https://jsonplaceholder.typicode.com/todos/1")
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((data) => console.log(data));
+}
+
+function loadUsers() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((data) => displayUsers(data));
+}
+
+function loadPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
+function displayUsers(data) {
+  console.log(data);
 }
