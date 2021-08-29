b407580405beb5f509021575752ac556edf58b6c
habibshrabon <snhabib777@gmail.com>
2021-08-29 20:18:00 +0600

    Call dynamic api load dynamic data to display

--- Module-33/api-example/js/countries.js
+++ Module-33/api-example/js/countries.js
@@ -13,15 +13,37 @@ const displayCountries = (countries) => {
 
   const countriesDiv = document.getElementById("countries");
   countries.forEach((country) => {
-    console.log(country);
+    // console.log(country);
     const div = document.createElement("div");
     div.classList.add("country");
-    const h3 = document.createElement("h3");
-    h3.innerText = country.name;
-    div.appendChild(h3);
-    const p = document.createElement("p");
-    p.innerText = country.capital;
-    div.appendChild(p);
+    div.innerHTML = `
+        <h3>${country.name}</h3>
+        <p>${country.capital}</p>
+        <button onclick="loadCountryByName('${country.name}')">Details</button>
+    `;
+    // const h3 = document.createElement("h3");
+    // h3.innerText = country.name;
+    // div.appendChild(h3);
+    // const p = document.createElement("p");
+    // p.innerText = country.capital;
+    // div.appendChild(p);
     countriesDiv.appendChild(div);
   });
+};
+
+const loadCountryByName = (name) => {
+  const url = `https://restcountries.eu/rest/v2/name/${name}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCountryDetail(data[0]));
+};
+
+const displayCountryDetail = (country) => {
+  console.log(country);
+  const countryDiv = document.getElementById("country-detail");
+  countryDiv.innerHTML = `
+    <h5>${country.name}</h5>
+    <p>Population: ${country.population}</p>
+    <img width="200" src="${country.flag}">
+  `;
 };
