cb40d42294e78dfbfa61a3fc7482bd806b18d337
habibshrabon <snhabib777@gmail.com>
2021-09-04 23:32:36 +0600

    just commit

---
+++ Module-38/browser-api/explore.js
@@ -0,0 +1,11 @@
+const name = 15;
+const student = {
+  name: "jolil",
+  friend: "borsha",
+  job: "Business",
+  makeMovie: function (name) {
+    console.log(name);
+  },
+};
+student.makeMovie("Idiot");
+document.getElementById("name");
